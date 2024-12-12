855aa436c125b5bd3ce9ea14535a99be68e6222a
Chance <bradfordchance@gmail.com>
2024-12-12 12:51:44 -0800

    adding new starter project

---
+++ ReactComponetsRefresher/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  //this is imperative javascript code
+  // const para = document.createElement('p');
+  // para.textContent = 'this is visible';
+  // document.getElementById('root').appendChild(para);
+
+  // this is declarative react code
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>this is also visible</p>
+    </div>
+  );
+}
+
+export default App;
